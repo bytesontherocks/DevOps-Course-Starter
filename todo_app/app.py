3a49e2477b7aa6d63d0af36be4f5ba43258abad5
bytesontherocks <bytes@dimoni>
2022-12-11 23:09:05 +0000

    index rendered and starting get_items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from todo_app.data.session_items import get_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/index')
 def index():
-    return 'Hello World!'
+    items=get_items()
+    return render_template('index.html', items=items)
