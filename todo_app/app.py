24030307d91ff17638508fa303488145fe79ea49
bytesontherocks <bytes@dimoni>
2022-12-11 23:55:01 +0000

    form with add title and posting working

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, redirect, url_for, render_template, request
+from todo_app.data.session_items import get_items,add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -9,4 +9,11 @@ app.config.from_object(Config())
 @app.route('/index')
 def index():
     items=get_items()
+    
     return render_template('index.html', items=items)
+
+@app.route('/index', methods=['POST'])
+def add_todo_item():
+    new_item_id = request.form.get('title')
+    add_item(new_item_id)
+    return redirect(url_for('index'))
