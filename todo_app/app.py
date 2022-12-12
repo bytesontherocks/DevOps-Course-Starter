4ffb66634fe1683012ab9d9a8f8a971202635a1d
bytesontherocks <bytes@dimoni>
2022-12-12 00:42:38 +0000

    playing with multiple buttons

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request
-from todo_app.data.session_items import get_items,add_item
+from todo_app.data.session_items import get_items,get_item,add_item,save_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -11,8 +11,17 @@ def index():
     items=get_items()    
     return render_template('index.html', items=items)
 
-@app.route('/index', methods=['POST'])
+@app.route('/add_todo_item', methods=['POST'])
 def add_todo_item():
-    new_item_title = request.form.get('title')
+    new_item_title = request.form.get('title')    
     add_item(new_item_title)
+    print("add_todo_item")
     return redirect(url_for('index'))
+
+@app.route('/item_completed', methods=['POST'])
+def item_completed():
+    completed_item_id = request.form.get('id')
+    save_item(get_item(completed_item_id))
+    print("item_completed")
+    return redirect(url_for('index'))
+
