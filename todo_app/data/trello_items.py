bd01e113fdbd15d80509a3553b4452bb6c9de759
BytesOnTheRocks <bytesontherocks@gmail.com>
2023-02-07 21:27:05 +0000

    chore:tidy up before adding class

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -16,7 +16,7 @@ query = {
   'token': trello_token
 }
 
-def get_lists():
+def _get_lists():
     
     url = f"https://api.trello.com/1/boards/{trello_board}/lists"
 
@@ -36,7 +36,7 @@ def get_lists():
     
     return names 
 
-def get_list_id(list_name):
+def _get_list_id(list_name):
 
     url = f"https://api.trello.com/1/boards/{trello_board}/lists"
 
@@ -55,11 +55,11 @@ def get_list_id(list_name):
             if l['name'] == list_name:
                 todo_list_id = l['id']
     else:
-        print(f"get_list_id request has failed. Status code: {response.status_code}")
+        print(f"_get_list_id request has failed. Status code: {response.status_code}")
 
     return todo_list_id
 
-def get_cards_in_list(list_id, list_name):
+def _get_cards_in_list(list_id, list_name):
     
     url = f"https://api.trello.com/1/lists/{list_id}/cards"
 
@@ -73,16 +73,16 @@ def get_cards_in_list(list_id, list_name):
     
     if response.status_code == 200:
         cards = json.loads(response.text)
-       
+
         for c in cards:
             item = {'id': c['idShort'], 'status': list_name, 'title': c['name']}
             items.append(item)        
     else:
-        print(f"get_cards_in_list request has failed. Status code: {response.status_code}")
+        print(f"_get_cards_in_list request has failed. Status code: {response.status_code}")
     
     return items
 
-def get_card_id(list_id, id_short : int):
+def _get_card_id(list_id, id_short : int):
     
     url = f"https://api.trello.com/1/lists/{list_id}/cards"
 
@@ -99,33 +99,11 @@ def get_card_id(list_id, id_short : int):
             if c['idShort'] == id_short:
                 return c['id']
     else:
-        print(f"get_card_id request has failed. Status code: {response.status_code}")
+        print(f"_get_card_id request has failed. Status code: {response.status_code}")
     
     return ""
 
-def move_card_to_new_list(card_short_id, current_list_name, new_list_name):
-
-    current_list_id = get_list_id(current_list_name)
-    new_list_id = get_list_id(new_list_name)
-    
-    card_id = get_card_id(current_list_id, card_short_id)
-
-    url = f"https://api.trello.com/1/cards/{card_id}"
-
-    # add destination list
-    query['idList'] = new_list_id
-    
-    print(f"moving card with Short ID {card_short_id} and long id {card_id} from old list id {current_list_id} to new list id {new_list_id}")
-
-    response = requests.request(
-        "PUT",
-        url,
-        headers=headers,
-        params=query
-    )
-
-
-def add_card(list_id, card_name):
+def _add_card(list_id, card_name):
 
     url = "https://api.trello.com/1/cards"
 
@@ -145,49 +123,24 @@ def add_card(list_id, card_name):
 
 def get_items():
     """
-    Fetches all saved items from the session.
+    Fetches items from the Trello API.
 
     Returns:
         list: The list of saved items.
     """
-    #get_lists()
-
-    # # Testing+
-    # todo_list_id = get_list_id("from_postman")
-    # pm_list_id = get_list_id( "To Do")
-
-    # move_card_to_new_list(todo_list_id, pm_list_id, 9)
-    # #
-
     all_items = []
-    lists_names = get_lists()
+    lists_names = _get_lists()
 
     for n in lists_names:
-        cards = get_cards_in_list(get_list_id(n),n)
+        cards = _get_cards_in_list(_get_list_id(n),n)
         for c in cards:
             all_items.append(c)
-
      
     return  all_items
 
-
-def get_item(id):
-    """
-    Fetches the saved item with the specified ID.
-
-    Args:
-        id: The ID of the item.
-
-    Returns:
-        item: The saved item, or None if no items match the specified ID.
-    """
-    items = get_items()
-    return next((item for item in items if item['id'] == int(id)), None)
-
-
 def add_item(title):
     """
-    Adds a new item with the specified title to the session.
+    Adds a new item with the specified title to the Trello board.
 
     Args:
         title: The title of the item.
@@ -195,20 +148,42 @@ def add_item(title):
     Returns:
         item: The saved item.
     """
-    todo_list_id = get_list_id("To Do")
-    add_card(todo_list_id, title)
+    todo_list_id = _get_list_id("To Do")
+    _add_card(todo_list_id, title)
 
-
-def save_item(item):
+def move_card_to_new_list(card_short_id, current_list_name, new_list_name):
     """
-    Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
+    Moves an specific card from one list to another
 
     Args:
-        item: The item to save.
+        card_short_id: short id of the card to move e.g. 14
+        current_list_name: name of the list that the card to move belongs to 'To Do'
+        new_list_name: name of the list that the card to move needs be moving to 'Done'
+
+    Returns:
+        item: void
     """
-    existing_items = get_items()
-    updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
+    current_list_id = _get_list_id(current_list_name)
+    new_list_id = _get_list_id(new_list_name)    
+    card_id = _get_card_id(current_list_id, card_short_id)
 
-    session['items'] = updated_items
+    #TODO: check errors
 
-    return item
+    url = f"https://api.trello.com/1/cards/{card_id}"
+
+    # add destination list
+    query['idList'] = new_list_id
+    
+    print(f"moving card with Short ID {card_short_id} and long id {card_id} from old list id {current_list_id} to new list id {new_list_id}")
+
+    response = requests.request(
+        "PUT",
+        url,
+        headers=headers,
+        params=query
+    )
+
+    if response.status_code == 200:
+        print("card moved successfully")
+    else:
+        print("Error moving card")
