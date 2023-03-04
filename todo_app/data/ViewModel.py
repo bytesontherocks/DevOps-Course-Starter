a50753ec1aa85f39c92ee9f8b5e57ffa39c9f9c8
BytesOnTheRocks <bytesontherocks@gmail.com>
2023-03-04 22:50:28 +0000

    ut passing

--- todo_app/data/ViewModel.py
+++ todo_app/data/ViewModel.py
@@ -13,3 +13,19 @@ class ViewModel:
             if it.status == "Done":
                 done_items.append(it)
         return done_items
+    
+    @property
+    def to_do_items(self):
+        to_do_items = []
+        for it in self._items:
+            if it.status == "To Do":
+                to_do_items.append(it)
+        return to_do_items
+    
+    @property
+    def doing_items(self):
+        doing_items = []
+        for it in self._items:
+            if it.status == "Doing":
+                doing_items.append(it)
+        return doing_items
--- todo_app/data/test/test_view_model.py
+++ todo_app/data/test/test_view_model.py
@@ -3,11 +3,15 @@ from  todo_app.data.Item import Item
 from  todo_app.data.ViewModel import ViewModel
 
 def test_view_model_done_property():
-    it = Item(1, "testing1", "Done")
-    vm = ViewModel(it)
+    items_to_test = []
+    items_to_test.append(Item(1, "testing1", "Done"))
+    items_to_test.append(Item(100, "Dibi", "To Do"))
+    items_to_test.append(Item(101, "Daba", "Done"))
+    vm = ViewModel(items_to_test)
     done_its = vm.done_items
 
-    assert(len(done_its) == 1)
-    assert(done_its[0] == it)
+    assert(len(done_its) == 2)
+    assert(done_its[0] == items_to_test[0])
+    assert(done_its[1] == items_to_test[2])
     
 
